9f0d0529aa12523590623e435f5a0cb0a22e7fa9
Jerem B <jrr87328@gmail.com>
2022-05-04 21:02:33 +0200

    commit 4

--- back/server.js
+++ back/server.js
@@ -3,23 +3,8 @@ const http = require('http');
 //importe le fichier app.js
 const app = require('./app');
 
-//fonction qui renvoi un numero de port valide qu'il soit en chaine de caractere ou en nombre
-const normalizePort = val => {
-  const port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
-};
-
-//appel de la fonction
-const port = normalizePort('3000');
 //utilisation de ce port
-app.set('port', port);
+app.set('port', 3000);
 
 //fonction qui gere les erreur
 const errorHandler = error => {
@@ -27,7 +12,7 @@ const errorHandler = error => {
     throw error;
   }
   const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + 3000;
   switch (error.code) {
     case 'EACCES':
       console.error(bind + ' requires elevated privileges.');
@@ -48,10 +33,10 @@ const server = http.createServer(app);
 server.on('error', errorHandler);
 server.on('listening', () => {
   const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + 3000;
   console.log('Listening on ' + bind);
 });
 
 //écoute du port sur lequel executer le serveur
-server.listen(port);
+server.listen(3000);
 
